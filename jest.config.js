11ccf3c79b55c7c2a049590aa7a605d056051390
Gustavo Teles <gustavo.teles711@gmail.com>
2025-08-20 22:33:07 -0300

    Configured test environment

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,18 @@
 export default {
-    testEnvironment: "jest-environment-jsdom",
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-    moduleNameMapper: {
-      "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
-      "\\.(css|less|sass|scss)$": "identity-obj-proxy",
-      "^@/(.*)$": "<rootDir>/src/$1"
-    },
-  };
+
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  transformIgnorePatterns: ['node_modules/(?!(jest-)?react)'],
+
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__mocks__/fileMock.js",
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+};
